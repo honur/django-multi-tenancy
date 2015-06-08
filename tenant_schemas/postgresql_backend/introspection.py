6a43f330e6aa1a133b2ab9035ea9baba3ad9f8bd
hov <hovikn@gmail.com>
2015-06-08 20:44:27 +0400

    updated select query in get_table_list as in django 1.8

--- tenant_schemas/postgresql_backend/introspection.py
+++ tenant_schemas/postgresql_backend/introspection.py
@@ -7,7 +7,7 @@ class DatabaseSchemaIntrospection(DatabaseIntrospection):
     def get_table_list(self, cursor):
         "Returns a list of table names in the current database and schema."
         cursor.execute("""
-            SELECT c.relname
+            SELECT c.relname, c.relkind
             FROM pg_catalog.pg_class c
             LEFT JOIN pg_catalog.pg_namespace n ON n.oid = c.relnamespace
             WHERE c.relkind IN ('r', 'v', '')
